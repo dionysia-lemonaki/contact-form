6c4497cfd1cd77dbd860468eb1ce311b8729d2b1
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-07-06 15:11:47 +0300

    feat: add RadioGroup component

---
+++ src/components/RadioGroup.tsx
@@ -0,0 +1,50 @@
+import type { UseFormRegister } from "react-hook-form";
+import { useId } from "react";
+import RadioGroupItem from "./RadioGroupItem";
+import type { ContactForm } from "../schemas/contactFormSchema";
+
+interface Radios {
+  id: "general" | "support";
+  title: string;
+}
+
+interface RadioGroupProps {
+  legend: string;
+  queryTypes: Radios[];
+  name: "queryType";
+  register: UseFormRegister<ContactForm>;
+  error?: string;
+}
+
+const RadioGroup = ({
+  legend,
+  queryTypes,
+  name,
+  register,
+  error,
+}: RadioGroupProps) => {
+  const id = useId();
+  return (
+    <fieldset aria-describedby={error ? id : undefined}>
+      <legend>{legend}</legend>
+      <div>
+        {queryTypes.map((queryType) => (
+          <RadioGroupItem
+            key={queryType.id}
+            label={queryType.title}
+            value={queryType.id}
+            register={register}
+            name={name}
+          />
+        ))}
+      </div>
+      {error && (
+        <p id={id} role="alert">
+          {error}
+        </p>
+      )}
+    </fieldset>
+  );
+};
+
+export default RadioGroup;
