12ecba410b66fa6faf2572db67354ea87e76d88e
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-08-21 15:14:28 +0300

    style: add App styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ export default function App() {
     setShowToast(false);
   };
   return (
-    <main>
-      <div>
-        <h1>Contact Us</h1>
+    <main className="min-h-screen flex justify-center items-center p-4">
+      <div className="bg-white max-w-184 w-full p-6 md:p-10 rounded-2xl flex flex-col gap-8">
+        <h1 className=" font-bold text-[2rem] leading-none">Contact Us</h1>
         <ContactForm onSuccess={handleSuccess} />
       </div>
       <Toast show={showToast} onClick={handleDismiss} />
