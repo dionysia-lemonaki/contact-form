adcabad734f4957a493f762256e34556a63b1fd1
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-08-21 13:05:50 +0300

    feat: wire up ContactForm in App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "./components/ContactForm";
+
+export default function App() {
+  return (
+    <main>
+      <div>
+        <h1>Contact Us</h1>
+        <ContactForm />
+      </div>
+    </main>
+  );
+}
