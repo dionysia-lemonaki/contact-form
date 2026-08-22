ee71ba99cad4e26b89cef735e3e5424c693688f4
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-08-22 13:56:12 +0300

    style: add Toast component styles

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -15,17 +15,27 @@ export default function Toast({
     }
   }, [show]);
   return (
-    <div role="status" aria-live="polite">
+    <div role="status" aria-live="polite" className="fixed top-0 p-6">
       {show && (
-        <div>
-          <div>
-            <Check size={24} aria-hidden="true" />
-            <p>Message Sent!</p>
+        <div className="bg-grey-900 text-white max-w-112.5 w-full rounded-xl flex flex-col gap-2 p-6">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2">
+              <Check
+                size={20}
+                className="border border-white rounded-full"
+                aria-hidden="true"
+              />
+              <p>Message Sent!</p>
+            </div>
+            <button
+              ref={dismissRef}
+              onClick={onClick}
+              className="flex items-center gap-2 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 cursor-pointer"
+            >
+              <X size={24} aria-hidden="true" />
+              <span className="sr-only">Dismiss</span>
+            </button>
           </div>
-          <button ref={dismissRef} onClick={onClick}>
-            <X size={24} aria-hidden="true" />
-            <span>Dismiss</span>
-          </button>
           <p>Thanks for completing the form. We'll be in touch soon!</p>
         </div>
       )}
