bbe9f55324bd1db5b24ce07bf7bdf2907f342943
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-08-22 12:28:18 +0300

    style: add CheckBox component styles

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -19,25 +19,32 @@ export default function CheckBox({
   const inputId = `${baseId}-input`;
   const errorId = `${baseId}-error`;
   return (
-    <div>
-      <div>
+    <div className="mb-10">
+      <div className="flex items-center gap-4 mb-2">
         <input
           type="checkbox"
           id={inputId}
           {...register(name)}
           aria-describedby={error ? errorId : undefined}
           aria-invalid={!!error}
+          className="accent-green-600"
         />
         <div>
-          <label htmlFor={inputId}>
-            <span>{label}</span>
-            <span aria-hidden={true}>*</span>
+          <label htmlFor={inputId} className="flex items-center gap-1">
+            <span className="text-base font-normal leading-6">{label}</span>
+            <span aria-hidden={true} className="text-green-600">
+              *
+            </span>
             <span className="sr-only">(required)</span>
           </label>
         </div>
       </div>
       {error && (
-        <p id={errorId} role="alert">
+        <p
+          id={errorId}
+          role="alert"
+          className="text-red text-base font-normal leading-6"
+        >
           {error}
         </p>
       )}
