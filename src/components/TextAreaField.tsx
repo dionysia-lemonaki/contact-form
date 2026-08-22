5ddd8dc1767cd6832eb4c5948b59dad7aeef8205
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-08-22 11:51:30 +0300

    style: add TextAreaField component styles

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -19,10 +19,12 @@ export default function TextAreaField({
   const inputId = `${baseId}-input`;
   const errorId = `${baseId}-error`;
   return (
-    <div>
-      <label htmlFor={inputId}>
-        <span>{label}</span>
-        <span aria-hidden={true}>*</span>
+    <div className="flex flex-col gap-2 mb-10">
+      <label htmlFor={inputId} className="flex items-center gap-2">
+        <span className="text-base font-normal leading-6">{label}</span>
+        <span aria-hidden={true} className="text-green-600">
+          *
+        </span>
         <span className="sr-only">(required)</span>
       </label>
       <textarea
@@ -30,9 +32,18 @@ export default function TextAreaField({
         aria-describedby={error ? errorId : undefined}
         aria-invalid={!!error}
         {...register(name)}
+        className={`border py-3 px-6 rounded-lg resize-none min-h-60 md:min-h-33 lg:min-h-26.25 hover:border-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 ${
+          error
+            ? "border-red focus-visible:outline-red"
+            : "border-grey-500 focus-visible:outline-green-600"
+        }`}
       ></textarea>
       {error && (
-        <p id={errorId} role="alert">
+        <p
+          id={errorId}
+          role="alert"
+          className="text-red text-base font-normal leading-6"
+        >
           {error}
         </p>
       )}
